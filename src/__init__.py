38b2853206e40942f8f82ceacd1487507276100e
“Matheus <1MHCTDS8@gmail.com>
2024-03-14 14:17:47 -0300

    Change to src

--- src/__init__.py
+++ src/__init__.py
@@ -1,26 +1,3 @@
-from setuptools import setup, find_packages
-
-VERSION = '0.0.1' 
-DESCRIPTION = 'Scripts for the essay classification'
-LONG_DESCRIPTION = 'My first Python package, scripts for the essay classification. Just for demonstration and testing, no actual support will be done in the future'
-
-setup(
-        name="src", 
-        version=VERSION,
-        author="Matheus Henrique Cajueiro Tobias de Souza",
-        author_email="1MHCTDS8@gmail.com",
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        packages=find_packages(),
-        install_requires=["matplotlib.pyplot","numpy","pandas","seaborn","sklearn","tqdm","nltk"],
-        
-        keywords=['python', 'first package'],
-        classifiers= [
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Education",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 3",
-            "Operating System :: MacOS :: MacOS X",
-            "Operating System :: Microsoft :: Windows",
-        ]
-)
+import features
+import models
+import visualization
